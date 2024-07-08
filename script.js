0bf58df7770778f038b11786e04291e008a57fca
PeriAksoy <nisaperiaksoy27@gmail.com>
2024-07-08 17:38:31 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 var number1 = document.getElementById("number1");
 var number2 = document.getElementById("number2");
-function addition(){
-var result = Number(number1.value) + Number(number2.value);
-document.getElementById("result").innerHTML = result;
-}
-function extraction(){
-var result = Number(number1.value) - Number(number2.value);
-document.getElementById("result").innerHTML = result;
-}
-function multiplication(){
-    var result = Number(number1.value) * Number(number2.value);
-    document.getElementById("result").innerHTML = result;
-}
-function division(){
-    var result = Number(number1.value) / Number(number2.value);
+function calculate(type){
+    var result = "";
+
+    switch(type){
+        case "+":
+            result=Number(number1.value) + Number(number2.value);
+            break;
+        case "-":
+            result=Number(number1.value) - Number(number2.value);
+            break;
+        case "*":
+            result=Number(number1.value) * Number(number2.value);
+            break;  
+        case "/":
+            result=Number(number1.value) / Number(number2.value);
+            break;  
+    }
     document.getElementById("result").innerHTML = result;
 }
